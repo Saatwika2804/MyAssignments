f2dd1ad0ff746ae527141c7e77b8dea902a69c20
Saatwika2804 <saatwika.s2804@gmail.com>
2025-04-26 22:17:48 +0530

    Add files via upload

---
+++ day1/HelloTestLeaf.java
@@ -0,0 +1,11 @@
+package week1.day1;
+
+public class HelloTestLeaf {
+	//type main and select ctrl+space
+	public static void main(String[] args) {
+		//type syso and press ctrl+space 
+		System.out.println("Welcome to Testleaf");
+		
+	}
+	
+}
